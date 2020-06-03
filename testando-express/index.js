b80c0ce62f9563564146bcb5ae935b388f7de716
GAMELEIRA <gameleira270499@gmail.com>
2020-06-03 02:37:33 -0300

    Criado um exemplo simples de como substituir as bibliotecas fracas http do node utilizando o framework express

---
+++ testando-express/index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/ola", (req, res) => {
+
+    res.send("");
+
+})
+
+app.get("/", (req, res) => {
+
+    const query = req.query["canal"];
+
+    console.log(query);
+
+    res.send("Bem vindo ao guia do programador!");
+
+});
+
+//ROTA OPCIONAL
+app.get("/gaby/:parceiro?", (req, res) => {
+
+    res.send(req.params.parceiro);
+
+});
+
+app.listen(3333, (erro) => {
+
+    if (!erro) {
+
+        console.log("Servidor startado!");
+
+    } else {
+
+        console.log("Servidor caiu!");
+    }
+
+});
