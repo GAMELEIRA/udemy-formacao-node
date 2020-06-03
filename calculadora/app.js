c75594f738e1573e83230b3d11a1d5e5434c560a
GAMELEIRA <gameleira270499@gmail.com>
2020-06-03 02:35:19 -0300

    Criado um exemplo simples de utilização de modulos no node

---
+++ calculadora/app.js
@@ -0,0 +1,23 @@
+const calculadora = require("./calculadora.js");
+
+console.log(calculadora.soma(10, 20));
+
+console.log(calculadora.multiplicacao(10, 20));
+
+console.log(calculadora.subtracao(10, 20));
+
+console.log(calculadora.divisao(10, 20));
+
+// const mostrarSite = false;
+
+// const site = "wwww.g1.com.br"
+
+// console.log("Hello world!");
+
+// console.log("Gabriel Gameleira dos Santos");
+
+// if (mostrarSite) {
+
+//     console.log(site);
+
+// }
---
+++ calculadora/calculadora.js
@@ -0,0 +1,27 @@
+module.exports = {
+
+    soma(a = 0, b = 0) {
+
+        return a + b;
+
+    },
+
+    multiplicacao(a = 0, b = 0) {
+
+        return a * b
+
+    },
+
+    divisao(a = 0, b = 0) {
+
+        return a / b;
+
+    },
+
+    subtracao(a = 0, b = 0) {
+
+        return a - b
+
+    }
+
+}
